b17bd4ac4a2250748ae61bcdecebc8264090412b
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-09-08 21:01:40 +0300

    add part 2 Lesson 16

--- Lesson_16-Part_2/ViewController.swift
+++ Lesson_16-Part_2/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
 
+//    cохранение через - FileManager (САМЫЙ РЕДКО используемый)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        чтение файла
+        guard let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("profileFolder"),
+              let data = try? Data(contentsOf: path) else {
+            return
+        }
+        let text = String(data: data, encoding: .utf8)
+        textField.text = text
+        
     }
 
+    @IBAction func buttonPressed() {
+        
+//        создание файла
+        guard let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("profileFolder")else {
+            return
+        }
+        
+        let text = textField.text
+        let data = text?.data(using: .utf8)
+        try? data?.write(to: path)
+        
+    }
 
 }
 
